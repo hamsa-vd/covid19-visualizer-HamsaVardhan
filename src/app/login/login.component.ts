eeca82797f4ebab2218d023ff37a5010caf2410d
Hamsa-Vardhan <hamsavardhan99@gmail.com>
2020-06-01 14:23:10 +0530

    added toaster

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validator, Validators } from '@angular/forms';
 import { ChildService } from '../child.service';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 @Component({
 	selector: 'app-login',
 	templateUrl: './login.component.html',
@@ -11,7 +12,12 @@ export class LoginComponent implements OnInit {
 	Register;
 	Login;
 	check = false;
-	constructor(private fb: FormBuilder, private serve: ChildService, private route: Router) {}
+	constructor(
+		private fb: FormBuilder,
+		private serve: ChildService,
+		private route: Router,
+		private toast: ToastrService
+	) {}
 
 	ngOnInit(): void {
 		this.Register = this.fb.group({
@@ -30,8 +36,10 @@ export class LoginComponent implements OnInit {
 			this.serve.logindata(this.Login.value).subscribe((v) => {
 				localStorage.setItem('token', v['token']);
 				this.route.navigate([ '/main' ]);
+				this.toast.success('successfully logged in');
 			});
 		else {
+			this.toast.error('Enter valid login details');
 		}
 	}
 	registersubmit() {
@@ -40,8 +48,10 @@ export class LoginComponent implements OnInit {
 			this.check = true;
 			this.serve.registerdata(this.Register.value).subscribe((v) => {
 				console.log(v);
+				this.toast.success('successfully registered');
 			});
 		} else {
+			this.toast.error('All fields are required');
 		}
 	}
 }
